5435e78aaff09dd5b7a6516f5f6ee9b446d1005d
BlackMooth <49418052+BlackMooth@users.noreply.github.com>
2019-05-13 10:51:20 +0200

    Update GA.cpp

--- GA.cpp
+++ GA.cpp
@@ -11,6 +11,8 @@ using namespace std;
 typedef vector<int> solution;
 typedef vector<solution> population;
 
+double COST = std::numeric_limits<double>::infinity();
+
 /* Tournament selection of parents; ILS with all Pair Swap search;
  * Elitism in population, keeping 50% of best solutions at every generation */
 
@@ -18,9 +20,9 @@ typedef vector<solution> population;
     int MAX_GENERATIONS = 3000;
     int TOURNAMENT_SIZE = 4;
     double CROSS_P = 0.7;
-    int WORSE_ACCEPTANCE_PROB = 0; //Prob of accepting worse solutions in local optimization process
+    int WORSE_ACCEPTANCE_PROB = 0.1; //Prob of accepting worse solutions in local optimization process
 
-bool comp(solution& s1, solution& s2){
+bool comp(vector<int>& s1, vector<int>& s2){
     return calculate_cost(s1, NUMBER_OBJECTS-1) < calculate_cost(s2, NUMBER_OBJECTS-1);
 }
 
@@ -174,95 +176,118 @@ solution crossover_opt(solution& p1, solution& p2){
 
 
 int main() {
-    inputData(DISTANCE, FLOW, NUMBER_OBJECTS);  
-    solution SOL(NUMBER_OBJECTS);
-    pop_size = 3; 
-    population SET_CHROMOSOMES = population (pop_size, solution(NUMBER_OBJECTS));
-    for (int i = 0; i < pop_size; i++) generateRandomSolution(SET_CHROMOSOMES[i], NUMBER_OBJECTS);
-    for (int i = 0; i < pop_size; i++) twoOptImprovement(SET_CHROMOSOMES[i], WORSE_ACCEPTANCE_PROB);
-    sort(SET_CHROMOSOMES.begin(), SET_CHROMOSOMES.end(), comp);
-    /*Selects the best solution*/  
-    solution best_solution(NUMBER_OBJECTS);
-    for (int i = 0; i < NUMBER_OBJECTS; i++) best_solution[i] = SET_CHROMOSOMES[0][i];
-    SOL = best_solution;
-    int iter_best = 0;
-    int u = 1, last_mutation = 0, mutation_type = 1, generation = 0;
+	srand(time(NULL));
+	vector<string> benchmarck;   //NEW
+	listing(benchmarck);		//NEW
+	for (int ind = 0; ind < benchmarck.size(); ind++) {
+		bool print_data = false;
+		std::stringstream ind_outp; 
+		ind_outp << benchmarck[ind] << ".result";  //output file will have .result extension
+		ofstream fout(ind_outp.str().c_str());
+		auto start = std::chrono::high_resolution_clock::now();
+		std::chrono::duration<double> elapsed;
+	    inputData(DISTANCE, FLOW, NUMBER_OBJECTS, benchmarck[ind]);  
+	    solution SOL(NUMBER_OBJECTS);
+	    pop_size = 3; 
+	    population SET_CHROMOSOMES = population (pop_size, solution(NUMBER_OBJECTS));
+	    do {
+		    for (int i = 0; i < pop_size; i++) generateRandomSolution(SET_CHROMOSOMES[i], NUMBER_OBJECTS);
+		    for (int i = 0; i < pop_size; i++) twoOptImprovement(SET_CHROMOSOMES[i], WORSE_ACCEPTANCE_PROB);
+		    sort(SET_CHROMOSOMES.begin(), SET_CHROMOSOMES.end(), comp);
+		    /*Selects the best solution*/  
+		    solution best_solution(NUMBER_OBJECTS);
+		    for (int i = 0; i < NUMBER_OBJECTS; i++) best_solution[i] = SET_CHROMOSOMES[0][i];
+		    SOL = best_solution;
+		    int iter_best = 0;
+		    int u = 1, last_mutation = 0, mutation_type = 1, generation = 0;
 
-    /* Main Loop */
-    while (generation < MAX_GENERATIONS) {
-        generation++;
-        vector<solution> offspring; //descendence of original chromosomes
-        while(offspring.size() < pop_size*0.5){
-            int except = -1;
-            /*Tournament to select parents*/
-            solution p1 = tournament(SET_CHROMOSOMES, MAX_GENERATIONS, except);
-            solution p2 = tournament(SET_CHROMOSOMES, MAX_GENERATIONS, except);
+		    auto start_10secs = std::chrono::high_resolution_clock::now();
+			std::chrono::duration<double> elapsed_10secs;
+		    /* Main Loop */
+		    while (generation < MAX_GENERATIONS) {
+		    	if (print_data) {start_10secs = std::chrono::high_resolution_clock::now(); print_data = false; fout << COST << endl;}
+		        generation++;
+		        vector<solution> offspring; //descendence of original chromosomes
+		        while(offspring.size() < pop_size*0.5){
+		            int except = -1;
+		            /*Tournament to select parents*/
+		            solution p1 = tournament(SET_CHROMOSOMES, MAX_GENERATIONS, except);
+		            solution p2 = tournament(SET_CHROMOSOMES, MAX_GENERATIONS, except);
 
-            /*crossover of parents*/
-            solution c = crossover_opt(p1, p2);
-            
-            /*Run a limited local search on the new child*/
-            c = limited_iterated_search(c);   
+		            /*crossover of parents*/
+		            solution c = crossover_opt(p1, p2);
+		            
+		            /*Run a limited local search on the new child*/
+		            c = limited_iterated_search(c);   
 
-            offspring.push_back(c);
-        }
+		            offspring.push_back(c);
+		        }
 
-        /*Sorts offspring in increasing order*/
-        sort(offspring.begin(), offspring.end(), comp);
-        /*Inserts the best individuals from offspring in the population as long as they are better than the latter*/
-        for(int i=0; i < offspring.size(); i++){
-            if(offspring[i] < SET_CHROMOSOMES[pop_size-1-i])
-                SET_CHROMOSOMES[pop_size-1-i] = offspring[i];
-        }
-        
-        /*Sorts population in increasing order*/
-        sort(SET_CHROMOSOMES.begin(), SET_CHROMOSOMES.end(), comp);
-        
-        /*In case population has converged*/
-        if(SET_CHROMOSOMES[0] >= best_solution && generation - last_mutation > 100 && generation - iter_best > 200 && mutation_type==1){ 
-            /*First try the shift mutation approach*/
-            for(int i=0; i < pop_size; i++){
-                shift_mutation(SET_CHROMOSOMES[i], u);
-            }
+		        /*Sorts offspring in increasing order*/
+		        sort(offspring.begin(), offspring.end(), comp);
+		        /*Inserts the best individuals from offspring in the population as long as they are better than the latter*/
+		        for(int i=0; i < offspring.size(); i++){
+		            if(offspring[i] < SET_CHROMOSOMES[pop_size-1-i])
+		                SET_CHROMOSOMES[pop_size-1-i] = offspring[i];
+		        }
+		        
+		        /*Sorts population in increasing order*/
+		        sort(SET_CHROMOSOMES.begin(), SET_CHROMOSOMES.end(), comp);
+		        
+		        /*In case population has converged*/
+		        if(calculate_cost(SET_CHROMOSOMES[0], NUMBER_OBJECTS-1) >= calculate_cost(best_solution, NUMBER_OBJECTS-1) && generation - last_mutation > 100 && generation - iter_best > 200 && mutation_type==1){ 
+		            /*First try the shift mutation approach*/
+		            for(int i=0; i < pop_size; i++){
+		                shift_mutation(SET_CHROMOSOMES[i], u);
+		            }
 
-            u++;
-            if(u > NUMBER_OBJECTS){
-                /*In case the shift mutation has not done the job, go to the second type*/
-                u=1;
-                mutation_type=2;
-            }
+		            u++;
+		            if(u > NUMBER_OBJECTS){
+		                /*In case the shift mutation has not done the job, go to the second type*/
+		                u=1;
+		                mutation_type=2;
+		            }
 
-            sort(SET_CHROMOSOMES.begin(), SET_CHROMOSOMES.end(), comp);
-            last_mutation = generation;
-        }else if(SET_CHROMOSOMES[0] >= best_solution and generation - last_mutation > 100 && generation - iter_best > 200 && mutation_type==2){
-            
-            for(int i=0; i < pop_size; i++){
-                for(int j=0; j < NUMBER_OBJECTS*0.2; j++){
-                    int r = generateRandomNumber(0, NUMBER_OBJECTS-1);
-                    int s = generateRandomNumber(0, NUMBER_OBJECTS-1);
-                    while(r == s) r = generateRandomNumber(0, NUMBER_OBJECTS-1);
+		            sort(SET_CHROMOSOMES.begin(), SET_CHROMOSOMES.end(), comp);
+		            last_mutation = generation;
+		        }else if(calculate_cost(SET_CHROMOSOMES[0], NUMBER_OBJECTS-1) >= calculate_cost(best_solution, NUMBER_OBJECTS-1) and generation - last_mutation > 100 && generation - iter_best > 200 && mutation_type==2){
+		            
+		            for(int i=0; i < pop_size; i++){
+		                for(int j=0; j < NUMBER_OBJECTS*0.2; j++){
+		                    int r = generateRandomNumber(0, NUMBER_OBJECTS-1);
+		                    int s = generateRandomNumber(0, NUMBER_OBJECTS-1);
+		                    while(r == s) r = generateRandomNumber(0, NUMBER_OBJECTS-1);
 
-                    exchange(SET_CHROMOSOMES[i], r, s);
-                }
-                
-                /*Improves the solutions locally*/
-                twoOptImprovement(SET_CHROMOSOMES[i], WORSE_ACCEPTANCE_PROB);
-            }
-            
-            sort(SET_CHROMOSOMES.begin(), SET_CHROMOSOMES.end(), comp);
-            last_mutation = generation;
-            mutation_type = 1;
-        }
-        
-        /*If the new best solution is better than the current one*/
-        if(calculate_cost(SET_CHROMOSOMES[0], NUMBER_OBJECTS-1) < calculate_cost(SOL, NUMBER_OBJECTS-1)){
-            /*Update the current best solution*/
-            SOL = SET_CHROMOSOMES[0];
-            iter_best = generation;
-            u = 1;
-        }
+		                    exchange(SET_CHROMOSOMES[i], r, s);
+		                }
+		                
+		                /*Improves the solutions locally*/
+		                twoOptImprovement(SET_CHROMOSOMES[i], WORSE_ACCEPTANCE_PROB);
+		            }
+		            
+		            sort(SET_CHROMOSOMES.begin(), SET_CHROMOSOMES.end(), comp);
+		            last_mutation = generation;
+		            mutation_type = 1;
+		        }
+		        
+		        /*If the new best solution is better than the current one*/
+		        if(calculate_cost(SET_CHROMOSOMES[0], NUMBER_OBJECTS-1) < calculate_cost(SOL, NUMBER_OBJECTS-1)){
+		            /*Update the current best solution*/
+		            SOL = SET_CHROMOSOMES[0];
+		            COST = calculate_cost(SOL, NUMBER_OBJECTS-1);
+		            iter_best = generation;
+		            u = 1;
+		            /* Print results */ 
+		        }
+				auto finish_10secs = std::chrono::high_resolution_clock::now();
+				elapsed_10secs = finish_10secs - start_10secs; 
+		        if (elapsed_10secs.count() >= 10) {print_data = true;}
+		    }
+		    auto finish = std::chrono::high_resolution_clock::now();
+		    elapsed = finish - start;
+		} while (elapsed.count() <=  600); //Mesurem 10 minuts  
+		fout << endl << "Best solution found is: ";
+		for (int i = 0; i < NUMBER_OBJECTS; i++) fout << SOL[i] << ' ';
+		fout << endl << "With cost: " << COST << endl;    
     }
-    cout << "Best solution found is: ";
-    for (int i = 0; i < NUMBER_OBJECTS; i++) cout << SOL[i] << ' ';
-    cout << endl << "With cost: " << calculate_cost(SOL, NUMBER_OBJECTS-1) << endl;    
 }   
