c479ac52835900919594a5e61e6aca2138b5e507
BlackMooth <49418052+BlackMooth@users.noreply.github.com>
2019-05-13 10:55:04 +0200

    Update tabu_search.cpp

--- tabu_search.cpp
+++ tabu_search.cpp
@@ -3,62 +3,49 @@
 #include <vector>
 #include <string.h>
 #include <time.h> 
+#include <limits>
 
 #include "../input_output.hh"
 #include "../random.hh"
 #include "../Qclass.hh"
+#include "../costs.hh"
 
 using namespace std;
 
 typedef int*   type_vector;
-typedef long** type_matrix;
+typedef double** type_matrix;
 
-const long infinity = std::numeric_limits<long>::infinity();
+const double infinity = std::numeric_limits<double>::infinity();
 
-const long NUMBER_ITERATIONS = 1000000;
+const double NUMBER_ITERATIONS = 1000000;
 
-void transpose(int & a, int & b) {long temp = a; a = b; b = temp;}
+void transpose(int & a, int & b) {double temp = a; a = b; b = temp;}
 
-int min(long a, long b) {if (a < b) return(a); else return(b);}
+int min(double a, double b) {if (a < b) return(a); else return(b);}
 
 
-
-/*--------------------------------------------------------------*/
-/*       compute the cost difference if elements i and j        */
-/*         are transposed in permutation (solution) p           */
-/*--------------------------------------------------------------*/
-long compute_delta(vector<int>& p, int i, int j) {
-  long d; int k;
-  d = (DISTANCE[i][i]-DISTANCE[j][j])*(FLOW[p[j]][p[j]]-FLOW[p[i]][p[i]]) +
-      (DISTANCE[i][j]-DISTANCE[j][i])*(FLOW[p[j]][p[i]]-FLOW[p[i]][p[j]]);
-  for (k = 0; k < NUMBER_OBJECTS; k++) if (k!=i and k!=j)
-    d = d + (DISTANCE[k][i]-DISTANCE[k][j])*(FLOW[p[k]][p[j]]-FLOW[p[k]][p[i]]) +
-            (DISTANCE[i][k]-DISTANCE[j][k])*(FLOW[p[j]][p[k]]-FLOW[p[i]][p[k]]);
-  return(d);
- }
-
-/*--------------------------------------------------------------*/
-/*      Idem, but the value of delta[i][j] is supposed to       */
-/*    be known before the transposition of elements r and s     */
-/*--------------------------------------------------------------*/
-long compute_delta_part(vector<int>& p, type_matrix & delta, int i, int j, int r, int s) {
+/*--------------------------------------------------------------------------*/
+/*      As calculateValueSwap, but the value of delta[i][j] is supposed to-*/
+/*    be known before the transposition of elements r and s     ----------*/
+/*-----------------------------------------------------------------------*/
+double compute_delta_part(vector<int>& p, type_matrix & delta, int i, int j, int r, int s) {
      return(delta[i][j]+(DISTANCE[r][i]-DISTANCE[r][j]+DISTANCE[s][j]-DISTANCE[s][i])*
      (FLOW[p[s]][p[i]]-FLOW[p[s]][p[j]]+FLOW[p[r]][p[j]]-FLOW[p[r]][p[i]])+
      (DISTANCE[i][r]-DISTANCE[j][r]+DISTANCE[j][s]-DISTANCE[i][s])*
      (FLOW[p[i]][p[s]]-FLOW[p[j]][p[s]]+FLOW[p[j]][p[r]]-FLOW[p[i]][p[r]]) );
   }
 
-void tabu_search(vector<int>& best_sol, long& best_cost, long min_size, long max_size, long aspiration) {
+void tabu_search(vector<int>& best_sol, double& best_cost, double min_size, double max_size, double aspiration) {
   vector<int> p(NUMBER_OBJECTS);                        // current solution
   type_matrix delta;                    // store move costs
   type_matrix tabu_list;                // tabu status
-  long current_iteration;               
-  long current_cost;                    
+  double current_iteration;               
+  double current_cost;                    
   int i, j, k, i_retained, j_retained;  // indices
-  delta = new long* [NUMBER_OBJECTS];
-  for (i = 0; i < NUMBER_OBJECTS; i++) delta[i] = new long[NUMBER_OBJECTS];
-  tabu_list = new long* [NUMBER_OBJECTS];
-  for (i = 0; i < NUMBER_OBJECTS; i++) tabu_list[i] = new long[NUMBER_OBJECTS];
+  delta = new double* [NUMBER_OBJECTS];
+  for (i = 0; i < NUMBER_OBJECTS; i++) delta[i] = new double[NUMBER_OBJECTS];
+  tabu_list = new double* [NUMBER_OBJECTS];
+  for (i = 0; i < NUMBER_OBJECTS; i++) tabu_list[i] = new double[NUMBER_OBJECTS];
 
   /* Current solution initialization */
   for (i = 0; i < NUMBER_OBJECTS; i++) p[i] = best_sol[i];
@@ -67,7 +54,7 @@ void tabu_search(vector<int>& best_sol, long& best_cost, long min_size, long max
   current_cost = 0;
   for (i = 0; i < NUMBER_OBJECTS; i++) for (j = 0; j < NUMBER_OBJECTS; j++){
     current_cost = current_cost + DISTANCE[i][j] * FLOW[p[i]][p[j]];
-    if (i < j) delta[i][j] = compute_delta(p, i, j);
+    if (i < j) delta[i][j] = calculateValueSwap(p, i, j, NUMBER_OBJECTS);
    }
   best_cost = current_cost;
 
@@ -79,7 +66,7 @@ void tabu_search(vector<int>& best_sol, long& best_cost, long min_size, long max
   for (current_iteration = 0; current_iteration < NUMBER_ITERATIONS; current_iteration++) {
   /** find best move (i_retained, j_retained) **/
     i_retained = infinity;       // in case all moves are tabu
-    long min_delta = infinity;   // retained move cost
+    double min_delta = infinity;   // retained move cost
     bool autorized;               // move not tabu?
     bool aspired;                 // move forced?
     bool already_aspired = false; // in case many moves forced
@@ -129,7 +116,7 @@ void tabu_search(vector<int>& best_sol, long& best_cost, long min_size, long max
             compute_delta_part(p, delta, 
                                i, j, i_retained, j_retained);}
         else
-         {delta[i][j] = compute_delta(p, i, j);}
+         {delta[i][j] = calculateValueSwap(p, i, j, NUMBER_OBJECTS);}
      }
       
    }
@@ -139,21 +126,17 @@ void tabu_search(vector<int>& best_sol, long& best_cost, long min_size, long max
   delete[] tabu_list;
 } // tabu
 
-void generate_random_solution(vector<int>& p) {
-  int i;
-  for (i = 0; i < NUMBER_OBJECTS; i++) p[i] = i;
-  for (i = 1; i <  NUMBER_OBJECTS; i++) transpose(p[i], p[generateRandomNumber(i, NUMBER_OBJECTS-1)]);
- }
-
 vector<int> solution;     // solution (permutation) 
-long cost;                // solution cost
+double cost;                // solution cost
 
 int main() {
+  /* Starting random number generator */
+  srand(time(NULL));
   inputData(DISTANCE, FLOW, NUMBER_OBJECTS);
   solution = vector<int>(NUMBER_OBJECTS);
-  generate_random_solution(solution);
+  generateRandomSolution(solution, NUMBER_OBJECTS);
   tabu_search(solution, cost, 9*NUMBER_OBJECTS/10, 11*NUMBER_OBJECTS/10, NUMBER_OBJECTS*NUMBER_OBJECTS*2); 
   cout << "Solution found by tabu search :\n";
   for (int i = 0; i < NUMBER_OBJECTS; i++) cout << solution[i] << ' '; 
-  cout << endl;
+  cout << endl << "With cost: " << calculate_cost(solution, NUMBER_OBJECTS-1) << endl;
  }
