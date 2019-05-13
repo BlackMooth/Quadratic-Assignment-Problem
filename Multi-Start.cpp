3ae055a5468277aed3dabb486e7947792d8b9d49
BlackMooth <49418052+BlackMooth@users.noreply.github.com>
2019-05-13 10:53:08 +0200

    Update Multi-Start.cpp

--- Multi-Start.cpp
+++ Multi-Start.cpp
@@ -5,15 +5,12 @@
 #include "../local_Improvement.hh"
 #include <list>
 #include <limits>
-#include <iostream>
 
 typedef vector<int> solution;
 typedef vector<solution> population;
 
 const double infinity = std::numeric_limits<double>::infinity();
 
-int K_sols = 15; //to be adjusted
-int MAX_ITERATIONS = 100000; //max #iterations in local improvement process, to be adjusted...
 int WORSE_ACCEPTANCE_PROB = 0; //to be adjusted
 
 solution SOL;
@@ -21,21 +18,39 @@ double COST = infinity;
 
 int main() {
 	srand(time(NULL));
-	inputData(DISTANCE, FLOW, NUMBER_OBJECTS);
-	solution Z(NUMBER_OBJECTS);
-	generateRandomSolution(Z, NUMBER_OBJECTS-1);
-	SOL = Z;
-	COST = calculate_cost(Z, NUMBER_OBJECTS-1);
+	vector<string> benchmarck;   //NEW
+	listing(benchmarck);		//NEW
+	bool print_data = false;
+	for (int ind = 0; ind < benchmarck.size(); ind++) {
+		std::stringstream ind_outp; 
+		ind_outp << benchmarck[ind] << ".result";  //output file will have .result extension
+		ofstream fout(ind_outp.str().c_str());
+		auto start = std::chrono::high_resolution_clock::now();
+		std::chrono::duration<double> elapsed;
+		auto start_10secs = std::chrono::high_resolution_clock::now();
+		std::chrono::duration<double> elapsed_10secs;
+		inputData(DISTANCE, FLOW, NUMBER_OBJECTS, benchmarck[ind]);
+		/* Initial solution */
+		solution Z(NUMBER_OBJECTS);
+		generateRandomSolution(Z, NUMBER_OBJECTS);
+		SOL = Z;
+		COST = calculate_cost(Z, NUMBER_OBJECTS-1);
+		do {	
+			if (print_data) {start_10secs = std::chrono::high_resolution_clock::now(); print_data = false; fout << COST << endl;}
 
-	for (int s = 1; s < K_sols; s++) {
-		generateRandomSolution(Z, NUMBER_OBJECTS-1);
-		for (int j = 0; j < MAX_ITERATIONS; j++) {
+			generateRandomSolution(Z, NUMBER_OBJECTS);
 			twoOptImprovement(Z, WORSE_ACCEPTANCE_PROB);
 			int X = calculate_cost(Z, NUMBER_OBJECTS-1); 
-			if (X < COST) COST = X; 
-		cout << "Best solution found is: ";
-		for (int i = 0; i < NUMBER_OBJECTS; i++) cout << SOL[i] << ' ';
-		cout << endl << "With cost: " << COST << endl;
-		}
+			if (X < COST) {COST = X; SOL = Z;} 
+			
+			auto finish_10secs = std::chrono::high_resolution_clock::now();
+			auto finish = std::chrono::high_resolution_clock::now();
+			elapsed_10secs = finish_10secs - start_10secs;
+			elapsed = finish - start;
+			if (elapsed_10secs.count() >= 10) {print_data = true;}
+		} while (elapsed.count() <=  600); //Mesurem 10 minuts
+		fout << endl << "Best solution found is: ";
+		for (int i = 0; i < NUMBER_OBJECTS; i++) fout << SOL[i] << ' ';
+		fout << endl << "With cost: " << COST << endl;
 	}
 }
